4d95a23b2f2793dbd17eded16d4c973fc759dd44
Gustavo Ferreira <70302908+Gussstavof@users.noreply.github.com>
2022-10-30 16:52:53 -0300

    list order

---
+++ src/Main.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        List<Person>  personList = new ArrayList<Person>();
+
+        Person person1 = new Person();
+        person1.setName("Maria");
+        person1.setAge(20);
+        personList.add(person1);
+
+        Person person2 = new Person();
+        person2.setName("João");
+        person2.setAge(18);
+        personList.add(person2);
+
+        Person person3 = new Person();
+        person3.setName("Mario");
+        person3.setAge(19);
+        personList.add(person3);
+
+
+        System.out.println("Sem ordem");
+        System.out.println(personList);
+        personList.sort(new PersonNameComparator());
+        System.out.println("Ordem alfabetica");
+        System.out.println(personList);
+        System.out.println("Ordem por idade");
+        personList.sort(new PersonAgeComparator());
+        System.out.println(personList);
+        Collections.reverse(personList);
+        System.out.println(personList);
+    }
+}
